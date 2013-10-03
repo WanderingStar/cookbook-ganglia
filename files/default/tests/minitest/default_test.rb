b59fcfee8d9bfb4cbe66ca079d3efae989734fd8
Ben Plunkert <ben.plunkert@blackboard.com>
2013-10-03 16:46:50 -0700

    adding basic testing

---
+++ files/default/tests/minitest/default_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/spec'
+
+describe_recipe 'ganglia::default' do
+  include MiniTest::Chef::Assertions
+  include Minitest::Chef::Context
+  include Minitest::Chef::Resources
+  include Chef::Mixin::ShellOut
+
+  describe 'installation' do
+
+    it 'runs ganglia-monitor' do
+      assert service('ganglia-monitor').must_be_running
+    end
+
+    it 'runs ganglia-monitor on boot' do
+      assert service('ganglia-monitor').must_be_enabled
+    end
+
+  end
+
+end
