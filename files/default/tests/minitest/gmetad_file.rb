40f11cb2fdb512ca90ec8230f775befa61e23015
Ben Plunkert <ben.plunkert@blackboard.com>
2013-10-08 14:47:52 -0700

    fixing test describe_recipe lines

--- files/default/tests/minitest/gmetad_file.rb
+++ files/default/tests/minitest/gmetad_file.rb
@@ -1,6 +1,6 @@
 require 'minitest/spec'
 
-describe_recipe 'ganglia::source' do
+describe_recipe 'ganglia::gmetad' do
   include MiniTest::Chef::Assertions
   include Minitest::Chef::Context
   include Minitest::Chef::Resources
