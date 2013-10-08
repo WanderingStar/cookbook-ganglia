e8e54b212c76b55f429f782101c5a7d84242155d
Ben Plunkert <ben.plunkert@blackboard.com>
2013-10-08 14:46:04 -0700

    updating tests for ganglia web

--- files/default/tests/minitest/web_test.rb
+++ files/default/tests/minitest/web_test.rb
@@ -1,6 +1,6 @@
 require 'minitest/spec'
 
-describe_recipe 'ganglia::source' do
+describe_recipe 'ganglia::web' do
   include MiniTest::Chef::Assertions
   include Minitest::Chef::Context
   include Minitest::Chef::Resources
@@ -15,13 +15,27 @@ describe_recipe 'ganglia::source' do
       package("php").must_exist
     end
   end
-  
-  it 'copies the web directory' do
-    directory('/var/www/html/ganglia').must_exist
-  end
 
-  it 'creates the conf file' do
-    file('/etc/ganglia-webfrontend/apache.conf').must_exist
+  describe 'installation' do
+  
+    it 'copies the web directory' do
+      directory('/var/www/html/ganglia').must_exist
+    end
+
+    it 'creates the conf file' do
+      file('/etc/ganglia-webfrontend/apache.conf').must_exist
+    end
+
+    it 'runs apache' do
+      service('httpd').must_be_running
+    end
+
+    it 'runs apache on boot' do
+      service('httpd').must_be_enabled
+    end
+
+    it 'downloads ganglia-web tarfile' do
+      file("/usr/src/ganglia-#{node[:ganglia][:web_version]}.tar.gz").must_exist
+    end
   end
- 
 end
