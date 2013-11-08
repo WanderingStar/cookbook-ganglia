87f70063484f8f8a21cedc7b91720dbff20f9753
Stanislav Bogatyrev <realloc@clodo.ru>
2013-11-08 20:06:25 +0400

    FC019: Access node attributes in a consistent manner

--- recipes/default.rb
+++ recipes/default.rb
@@ -37,7 +37,7 @@ directory "/etc/ganglia"
 
 case node[:ganglia][:unicast]
 when true
-  server_hosts = node[:ganglia][:server_addresses] || search(:node, "role:#{node['ganglia']['server_role']} AND chef_environment:#{node.chef_environment}").map {|node| node.ipaddress}
+  server_hosts = node[:ganglia][:server_addresses] || search(:node, "role:#{node[:ganglia][:server_role]} AND chef_environment:#{node.chef_environment}").map {|node| node[:ipaddress]}
   if server_hosts.empty? 
      server_hosts = [ "127.0.0.1" ]
   end
